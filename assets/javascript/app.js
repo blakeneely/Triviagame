e6447acc8c26c9ce91199a3d42c2915691326e36
Blake Neely <blakeneely21@gmail.com>
2019-08-20 12:54:51 -0500

    loading base game without timer or style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    $("#start-btn").on("click", function(){                 // Removes start button on click to begin game
-        this.remove();
-    })
+    var counter = 30;
+    var timer = 0;
+    var currentQuestion = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var answerGuessed = false;
 
     var questions =[                                        // Array of question objects
         {
@@ -35,7 +38,7 @@ $(document).ready(function() {
             answer: "Jack Sparrow"
         },
         {
-            question: "Mickey Mouse's pet dog is named:",
+            question: "What is Mickey Mouse's pet dog named:",
             choices: ["Goofy", "Minnie", "Donald", "Pluto"],
             answer: "Pluto"
         },
@@ -54,9 +57,47 @@ $(document).ready(function() {
             choices: ["Rosa", "Unicorn", "Felicia", "Eleanor"],
             answer: "Eleanor"
         },
+    ];
+
+    function renderQuestion() {                                         // Function to load question and answers to html
+        var question = questions[currentQuestion].question;             // Assigns question value from questions array to question variable
+        var choicesArray = questions[currentQuestion].choices;          // Assigns choices value from choices array to choicesArray variable
+        var htmlChoices = "";                                           // Initializes choice variable with empty string
+        for(var i = 0; i < choicesArray.length; i++) {                  // Loops to add each choice from choiceArray with a p tag
+            htmlChoices += "<p class='htmlChoices'>" + choicesArray[i] + "</p>";    // Add class to p for CSS styling
+        }
+        $("#time").html("Timer: " + counter);                           // Adds timer to html
+        $("#game").html("<h2>" + question + "</h2>" + htmlChoices);     // Adds question and choices to html
+        if(currentQuestion === 9) {                                    // Ends game after 10 questions
+            $("#game").html("<h2>You did it!</h2><p>You got " + correct + " correct</p>");      //Displays how many correct and incorrect
+        }    
+    };
+
+    function checkAnswer() {
+        var answer = questions[currentQuestion].answer;                 // Stores current question answer value to answer variable
+        $(".htmlChoices").on("click", function(){                       // On click checks if text from html element matches answer varible
+            if($(this).text() === answer) {
+                correct++;                                              // Increase correct count
+                alert("bang");
+            }
+            else {
+                incorrect++;                                            // Increase incorrect count
+                alert("wrong choice");
+            }
+            currentQuestion++;                                          // Increase currentQuestion count
+            renderQuestion();
+            checkAnswer();
+        });
+    };
 
 
-    ]
+
+    $("#start-btn").on("click", function(){                             // Removes start button on click to begin game
+        this.remove();
+        renderQuestion();                                               // Begins game by loading first question
+        checkAnswer();
+    });
+
 
 
 });
