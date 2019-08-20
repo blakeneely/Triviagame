e2f7806eb02cf1092e2605732ef90907d1c2221d
Blake Neely <blakeneely21@gmail.com>
2019-08-20 14:30:42 -0500

    game runs 1 question then stalls on next

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
-    var counter = 30;
-    var timer = 0;
+    var counter = 15;
+    var timer;
+    var revealCounter = 5;
+    var revealTimer;
     var currentQuestion = 0;
     var correct = 0;
     var incorrect = 0;
-    var answerGuessed = false;
 
     var questions =[                                        // Array of question objects
         {
             question: "What makes time travel possible?",
             choices: ["Skynet", "Flux Capacitor", "Plutonium", "A wish"],
-            answer: "Flux Capacitor"
+            answer: "Flux Capacitor",
+            image: "assets/images/flux-capacitor.gif" 
         },
         {
             question: "Who directed Forrest Gump?",
             choices: ["Robert Zemeckis", "Steven Spielberg", "Ron Howard", "Nora Ephron"],
-            answer: "Robert Zemeckis"
+            answer: "Robert Zemeckis",
+            image: "assets/images/forrestgump.jpg"
         },
         {
             question: "Who is the killer in Friday the 13th(1980)?",
@@ -59,7 +62,54 @@ $(document).ready(function() {
         },
     ];
 
+    function revealCount() {
+        revealCounter--;
+        if (revealCounter === 0) {
+            renderQuestion();
+        }
+    };
+
+    function count() {
+        counter--;                                                      // Deducts 1 from counter each interval
+        $("#time").html("Timer: " + counter);                           // Updates html with current counter value with each interval
+        if (counter === 0) {                                            // Checks if time runs out
+            clearInterval(timer);                                       // Stops timer
+            incorrect++;                                                // Increases incorrect count
+            currentQuestion++;                                          // Increases currentQuestion count
+            renderQuestion();                                           // Moves on to next question
+        }
+    };
+
+    function showAnswer() {
+        var image = questions[currentQuestion].image;                   // Stores current question image to local image variable
+        clearInterval(timer);                                           // Stops question timer
+        $("#time").html(" ");                                           // Remove timer from html
+        revealTimer = setInterval(revealCount, 1000);                   // Start reveal count
+        $("#game").html("<img src=" + image + ">");                     // Show correct answer image on html
+        // renderQuestion();
+    };
+
+    function checkAnswer() {
+        var answer = questions[currentQuestion].answer;                 // Stores current question answer value to local answer variable
+        $(".htmlChoices").on("click", function(){                       // On click checks if text from html element matches answer varible
+            if($(this).text() === answer) {
+                correct++;                                              // Increase correct count
+            }
+            else {
+                incorrect++;                                            // Increase incorrect count
+            }
+            showAnswer();
+            currentQuestion++;                                          // Increase currentQuestion count
+            // renderQuestion();                                           // Loads next question
+            // checkAnswer();                                              // checks for answer
+        });
+    };
+
+
     function renderQuestion() {                                         // Function to load question and answers to html
+        counter = 15;                                                   // Reset counter back to 15 with every question
+        timer = setInterval(count, 1000);
+        clearInterval(revealTimer);
         var question = questions[currentQuestion].question;             // Assigns question value from questions array to question variable
         var choicesArray = questions[currentQuestion].choices;          // Assigns choices value from choices array to choicesArray variable
         var htmlChoices = "";                                           // Initializes choice variable with empty string
@@ -68,29 +118,10 @@ $(document).ready(function() {
         }
         $("#time").html("Timer: " + counter);                           // Adds timer to html
         $("#game").html("<h2>" + question + "</h2>" + htmlChoices);     // Adds question and choices to html
-        if(currentQuestion === 9) {                                    // Ends game after 10 questions
+        if(currentQuestion === 9) {                                     // Ends game after 10 questions
             $("#game").html("<h2>You did it!</h2><p>You got " + correct + " correct</p>");      //Displays how many correct and incorrect
         }    
     };
-
-    function checkAnswer() {
-        var answer = questions[currentQuestion].answer;                 // Stores current question answer value to answer variable
-        $(".htmlChoices").on("click", function(){                       // On click checks if text from html element matches answer varible
-            if($(this).text() === answer) {
-                correct++;                                              // Increase correct count
-                alert("bang");
-            }
-            else {
-                incorrect++;                                            // Increase incorrect count
-                alert("wrong choice");
-            }
-            currentQuestion++;                                          // Increase currentQuestion count
-            renderQuestion();
-            checkAnswer();
-        });
-    };
-
-
 
     $("#start-btn").on("click", function(){                             // Removes start button on click to begin game
         this.remove();
